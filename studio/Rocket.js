2d7dbffca223d77c2c5fac46a67fed23135846ff
Rebecca Pavelka <pavelka6@sbcglobal.net>
2019-10-18 21:06:03 -0500

    Typscript studio works!

--- studio/Rocket.js
+++ studio/Rocket.js
@@ -1,15 +1,45 @@
+"use strict";
+exports.__esModule = true;
 var Rocket = /** @class */ (function () {
     function Rocket(name, totalCapacityKg) {
+        this.cargoItems = [];
+        this.astronauts = [];
         this.name = name;
         this.totalCapacityKg = totalCapacityKg;
     }
     Rocket.prototype.sumMass = function (items) {
+        // Returns the sum of all items using each item's massKg property
+        var sumOfAllItems = 0;
+        for (var i = 0; i < items.length; i++) {
+            sumOfAllItems += items[i].massKg;
+        }
+        return sumOfAllItems;
     };
     Rocket.prototype.currentMassKg = function () {
+        return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
     };
     Rocket.prototype.canAdd = function (item) {
+        return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
     };
     Rocket.prototype.addCargo = function (cargo) {
+        if (this.canAdd(cargo)) {
+            this.cargoItems.push(cargo);
+            return true;
+        }
+        else {
+            return false;
+        }
+    };
+    Rocket.prototype.addAstronaut = function (astronaut) {
+        // Call canAdd method with astronaut array passed in to see if enough space to add astronaut to rocket
+        if (this.canAdd(astronaut)) {
+            this.astronauts.push(astronaut);
+            return true;
+        }
+        else {
+            return false;
+        }
     };
     return Rocket;
 }());
+exports.Rocket = Rocket;
--- studio/Rocket.ts
+++ studio/Rocket.ts
@@ -1,8 +1,13 @@
-class Rocket {
+import { Payload } from './Payload';
+import { Astronaut } from './Astronaut';
+import { Cargo } from './Cargo';
+
+export class Rocket {
     name: string;
     totalCapacityKg: number;
-    cargoItems: string[];
-    astronauts: string[];
+    cargoItems: Cargo[] = [];
+    astronauts: Astronaut[] = [];
+    massKg: number;
   
     constructor(name: string, totalCapacityKg: number) {
       this.name = name;
@@ -10,18 +15,38 @@ class Rocket {
     }
   
     sumMass( items: Payload[] ): number {
-
+      // Returns the sum of all items using each item's massKg property
+      let sumOfAllItems: number = 0;
+      for (let i = 0; i < items.length; i++) {
+        sumOfAllItems += items[i].massKg;
+      }
+      return sumOfAllItems;
     }
 
     currentMassKg(): number {
-
+      return this.sumMass(this.cargoItems) + this.sumMass(this.astronauts);
     }
 
     canAdd(item: Payload): boolean {
-
+      return (this.currentMassKg() + item.massKg) <= this.totalCapacityKg;
     }
 
     addCargo(cargo: Cargo) {
+      if (this.canAdd(cargo)) {
+        this.cargoItems.push(cargo);
+        return true;
+      } else {
+          return false;
+      }
+    }
 
+    addAstronaut(astronaut: Astronaut) {
+      // Call canAdd method with astronaut array passed in to see if enough space to add astronaut to rocket
+      if (this.canAdd(astronaut)) {
+        this.astronauts.push(astronaut);
+        return true;
+      } else {
+          return false;
+      }
     }
 }
